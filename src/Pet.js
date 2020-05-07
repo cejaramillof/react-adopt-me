6e79edfa4b1b9a61a765b32611eea2d49201e05f
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-05-06 19:01:28 -0500

    configure eslint for react

--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,19 @@
-import { createElement } from 'react';
+import React, { createElement } from 'react';
 
 // is better export like a function, for track error's in stack errors
 export default function Pet({ name, animal, breed }) {
+  /*
   return createElement("div", {}, [
     createElement("h1", {}, name),
     createElement("h2", {}, animal),
     createElement("h2", {}, breed),
   ]);
+  */
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 };

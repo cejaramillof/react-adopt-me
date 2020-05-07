178c9034c056e0e30c71c6d99af83c5dfe788a2e
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-05-06 20:51:29 -0500

    calling pet api

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet"; // when you add library here, and parcel is runing, he go installing without you need write npm install
 
 const SearchParams = () => {
   // const location = "Seattle, WA"; // with this, cant be changed in html, because on change he will re render again, and have this value.
   // The two data binding not is free on react. Like Angular
   const [location, setLocation] = useState("Seattle, WA"); // useState never can be inside loops or conditionals (ifs) because he expect to be called in the same order, in each render.
+
+  const [animal, setAnimal] = useState("dog");
   return (
     // is className and not class, because is a reserved word in javascript (like let or const), for create a Class.
     <div className="search-params">
@@ -17,6 +20,24 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            placeholder="Animal"
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            {/* onBlur is better way in this case than onChange, for screen readers */}
+            <option>All</option>
+            {ANIMALS.map((animal, index) => (
+              <option key={index} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
